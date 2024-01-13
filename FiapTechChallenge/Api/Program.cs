e683dfe9043295420d99a609704aa662fb758d82
Leonardo Carvalho Khouri <lkhouri@tecnologia-unica.com.br>
2024-01-12 22:06:30 -0300

    Inclusão do arquivo program, que não sei oq aconteceu

---
+++ FiapTechChallenge/Api/Program.cs
@@ -0,0 +1,28 @@
+﻿global using FastEndpoints;
+using FastEndpoints.Swagger;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddSimpleConsole(options =>
+{
+    options.TimestampFormat = "hh:mm:ss ";
+});
+
+builder.Services.AddFastEndpoints();
+
+builder.Services.SwaggerDocument(o =>
+{
+    o.DocumentSettings = s =>
+    {
+        s.DocumentName = "swagger";
+        s.Title = "Restaurant FIAP";
+        s.Version = "v1";
+        s.Description = "Documentation about endpoints";
+    };
+
+    o.EnableJWTBearerAuth = false;
+    o.ShortSchemaNames = false;
+    o.RemoveEmptyRequestSchema = true;
+});
+
+var app = builder.Build();
+app.Run();
